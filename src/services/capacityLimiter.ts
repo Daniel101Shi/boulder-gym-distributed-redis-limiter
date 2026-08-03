9fb8ddee78bd5d026d8f2aa3580d4ff63d864a5b
Daniel <daniel101shi@gmail.com>
2026-08-02 19:19:41 -0700

    redis logic

---
+++ src/services/capacityLimiter.ts
@@ -0,0 +1,38 @@
+import redis from '../config/redis.ts';
+import pool from '../config/db.ts';
+
+const SLOT_SECONDS = 3600;
+const TTL_CACHE = 3600;
+
+async function getMaxCap(wallId: string): Promise<number> {
+    const cacheKey = `wall:${wallId}:max_capacity` //redis key
+    const cached = await redis.get(cacheKey);
+
+    if (cached){
+        return parseInt (cached, 10); //parse as base 10
+    }
+
+    const result = await pool.query('SELECT max_capacity FROM walls WHERE id = $1', [wallId]); //max capcity of a wall set to expire @ TTL_CACHE
+    const maxCap = result.rows[0].max_capacity;
+
+    redis.set(cacheKey, maxCap.toString(), 'EX', TTL_CACHE);
+
+    return maxCap;
+}
+
+async function tryBookSlot(wallId: string, timeSlot: string): Promise<boolean> {
+    const maxCap = await getMaxCap(wallId);
+    const capKey = `capacity:${wallId}:${timeSlot}`; 
+
+    const newCount = await redis.incr(capKey); //incrment headcount
+
+    if (newCount === 1){
+        await redis.expire(capKey, TTL_CACHE); //headcount set to expire @ TTL Cache
+    }
+
+    if(newCount > maxCapacity){
+        await redis.decr(capKey);
+        return false;
+    }
+    return true;
+}
